4fc4db4786b6eb171c0fc47db3ed59af5b6f2a91
27janani <sce22is056@sairamtap.edu.in>
2025-04-29 22:25:16 +0530

    variables

--- My smart contract.sql
+++ My smart contract.sql
@@ -1,6 +1,12 @@
 pragma solidity ^0.8.0;
 
 contract MyContract {
+  string public myString  = "My string";
+  bool public boolean1  ="true";
+  uint public myUnit = 1;
+  int public myInt = 1;
+  address public myAddress = 0x3b9aB87DEcaABC02284FAbf6083376Da6eC9B670
 
-    
+
+
 }
