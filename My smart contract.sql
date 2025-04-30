11096fe1b366c3090d1c4272272a81989ee70c0c
27janani <sce22is056@sairamtap.edu.in>
2025-04-30 20:42:46 +0530

    function

--- My smart contract.sql
+++ My smart contract.sql
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 contract MyContract {
@@ -9,4 +10,17 @@ contract MyContract {
 
 
 
+}
+
+
+contract SimpleStorage {
+    uint public data;
+
+    function set(uint _data) public {
+        data = _data;
+    }
+
+    function get() public view returns (uint) {
+        return data;
+    }
 }
