15f0251c21f28e90cdd936a7d5ba49a827b5914b
27janani <sce22is056@sairamtap.edu.in>
2025-05-02 22:29:19 +0530

    access

--- My smart contract.sql
+++ My smart contract.sql
@@ -64,3 +64,26 @@ contract Voting {
         return votes[candidate];
     }
 }
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract AccessControl {
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Access denied!");
+        _;
+    }
+
+    uint public value;
+
+    function setValue(uint _value) public onlyOwner {
+        value = _value;
+    }
+}
