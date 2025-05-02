a47b6aed9c23ab1627ae35f6888aa648139c2220
27janani <sce22is056@sairamtap.edu.in>
2025-05-02 22:09:15 +0530

    voting

--- My smart contract.sql
+++ My smart contract.sql
@@ -48,3 +48,19 @@ contract Wallet {
         return address(this).balance;
     }
 }
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Voting {
+    mapping(string => uint) public votes;
+
+    function vote(string memory candidate) public {
+        votes[candidate] += 1;
+    }
+
+    function getVotes(string memory candidate) public view returns (uint) {
+        return votes[candidate];
+    }
+}
