e945dd688f12efba18f52ceed7e5f1a5d85f4ff0
27janani <sce22is056@sairamtap.edu.in>
2025-05-03 22:04:06 +0530

    contract

--- My smart contract.sql
+++ My smart contract.sql
@@ -199,3 +199,19 @@ contract SimpleToken {
     }
 }
 
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Animal {
+    function sound() public pure virtual returns (string memory) {
+        return "Some sound";
+    }
+}
+
+contract Dog is Animal {
+    function sound() public pure override returns (string memory) {
+        return "Bark!";
+    }
+}
+
