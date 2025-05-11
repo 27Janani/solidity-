57092d5f246ba6ac395b5e381cff6512c41a6609
27janani <sce22is056@sairamtap.edu.in>
2025-05-11 21:53:48 +0530

    Escrow

--- My smart contract.sol
+++ My smart contract.sol
@@ -320,3 +320,24 @@ contract Lottery {
         players = new address ; // reset
     }
 }
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Escrow {
+    address public buyer;
+    address public seller;
+    uint public amount;
+
+    constructor(address _seller) payable {
+        buyer = msg.sender;
+        seller = _seller;
+        amount = msg.value;
+    }
+
+    function release() public {
+        require(msg.sender == buyer, "Only buyer can release funds");
+        payable(seller).transfer(amount);
+    }
+}
