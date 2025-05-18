285842873de896ce7fefdfbca64653775749e398
27janani <sce22is056@sairamtap.edu.in>
2025-05-18 22:50:01 +0530

    mit commit

--- My smart contract.sol
+++ My smart contract.sol
@@ -499,3 +499,20 @@ contract SplitPayment {
         }
     }
 }
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public count;
+
+    function increment() public {
+        count++;
+    }
+
+    function decrement() public {
+        require(count > 0, "Already zero");
+        count--;
+    }
+}
