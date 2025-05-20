70cbd92fdeb2ec39a747960b2e244698793bef61
27janani <sce22is056@sairamtap.edu.in>
2025-05-20 23:30:15 +0530

    proxy

--- My smart contract.sol
+++ My smart contract.sol
@@ -573,3 +573,34 @@ contract BasicNFT {
         totalSupply++;
     }
 }
+
+
+// Logic contract
+contract CounterLogic {
+    uint public count;
+
+    function increment() public {
+        count++;
+    }
+}
+
+// Proxy contract
+contract Proxy {
+    address public logic;
+
+    constructor(address _logic) {
+        logic = _logic;
+    }
+
+    fallback() external payable {
+        address impl = logic;
+        assembly {
+            calldatacopy(0, 0, calldatasize())
+            let result := delegatecall(gas(), impl, 0, calldatasize(), 0, 0)
+            returndatacopy(0, 0, returndatasize())
+            switch result
+            case 0 { revert(0, returndatasize()) }
+            default { return(0, returndatasize()) }
+        }
+    }
+}
